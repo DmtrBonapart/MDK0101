c6b27e8909a666171b41e2c0df77d0ce1cd0bbda
DmtrBonapart <bonapartdmitriy@gmail.com>
2026-09-19 17:17:14 +0900

    первые задания

---
+++ 1.cs
@@ -0,0 +1,14 @@
+// //ввод 
+// Console.Write("введи длину: ");
+// double a = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("введи ширину: ");
+// double b = Convert.ToDouble(Console.ReadLine());
+
+// //считаю площадь и периметр
+// double s = a * b;
+// double p = 2 * (a + b);
+
+// //вывод 
+// Console.WriteLine($"площадь: {s}");
+// Console.WriteLine($"периметр: {p}");
---
+++ 2.cs
@@ -0,0 +1,9 @@
+// //ввод
+// Console.Write("введи количество рублей: ");
+// double rub = Convert.ToDouble(Console.ReadLine());
+// double kurs = 80;
+
+// //перевод в доллары
+// double usd = rub / kurs;
+// //вывод
+// Console.WriteLine($"доллары: {usd}");
