7e6604edacc81e829ad57fda17944663b28e4f32
DmtrBonapart <bonapartdmitriy@gmail.com>
2026-09-22 19:57:01 +0900

    доделал 3, 4, 5

---
+++ 3.cs
@@ -0,0 +1,14 @@
+// double sum = 0;
+// //ввожу температуру за каждый день недели
+// for (int i = 1; i <= 7; i++)
+// {
+//     Console.Write($"введи темпу за {i} день: ");
+//     //перевожу текст в число
+//     double temp = Convert.ToDouble(Console.ReadLine());
+//     //добавляю темпу к общей сумме
+//     sum += temp;
+// }
+// //считаю среднюю за неделю
+// double average = sum / 7;
+
+// Console.WriteLine($"средняя температура: {average}");
---
+++ 5.cs
@@ -0,0 +1,25 @@
+int[,] matrix = new int[3, 3];
+
+//ввожу элементы матрицы
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write($"введи элемент [{i},{j}]: ");
+        //перевожу текст в число
+        matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("новая матрица:");
+
+//вывожу изменённую матрицу
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write($"{matrix[j, i]} ");
+    }
+
+    Console.WriteLine();
+}
